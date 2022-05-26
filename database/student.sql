44ef9deb08de5f96e9f74c1672b8684edb8a422d
Akanksha-Maurya <97236755+Akanksha-Maurya@users.noreply.github.com>
2022-05-27 01:05:42 +0530

    Initial Commit

--- database/student.sql
+++ database/student.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE IF NOT EXISTS `myproject` /*! 40100 DEFAULT CHARACTER SET utf8 */;
+USE `myproject`;
 -- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
 --
 -- Host: localhost    Database: myproject
